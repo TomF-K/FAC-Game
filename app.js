f4f9ce6752939a88a3863d00edef8fded44097a8
Tom Forman-Kenrick <tom.kenrick@gmail.com>
2023-11-22 15:32:23 +0000

    fixed checkWord functionality

--- app.js
+++ app.js
@@ -12,33 +12,58 @@ const display = document.getElementById("display");
 document.querySelectorAll(".letter").forEach(tile => {
     tile.addEventListener("click", event => {
         tile.classList.toggle("clicked");
-
-// push tile value into guess array
+        
+        // push tile value into guess array
         guess.push(tile.textContent);
         // console.log(guess);
-
-// convert active letters into single string
-
-    display.innerHTML = guess.join("");
-    
+        
+        // convert active letters into single string
+        
+        display.innerHTML = guess.join("");
+        
     })
-
+    
 })
+
+let attempt = guess.toString();
 
 // Submit words 
 
 const submitBtn = document.getElementById("submit");
 
-let attempt = [];
+// Check if attempt exists in word list
+
+function checkWord(wordList, attempt) {
+    let success = false;
+        for (var i = 0; i < wordList.length; i++) {
+            if (wordList[i] === attempt) {
+                success = true;
+            } 
+    }
+    console.log('success?', success);
+}
+    
 
 submitBtn.addEventListener("click", function() {
-    attempt.push(display.innerHTML);
+    attempt += display.innerHTML.toLowerCase();
     console.log(attempt);
-// reset tiles 
-// reset display
-});
-
 // check if word exists in wordList
+checkWord(wordList, attempt);
+// reset tiles 
+        
+// reset display and tiles
+    display.innerHTML = "";
+    attempt = '';
+    guess = [];
+    document.querySelectorAll(".letter").forEach(tile => {
+        tile.classList.remove("clicked");
+    });
+    console.log(attempt);
+}); 
+
+
+
+
 
 
 
